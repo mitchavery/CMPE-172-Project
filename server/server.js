9483046405a5627d2e25d63a5a5dd23eba0030a7
mitchavery <avery.mitch651@gmail.com>
2019-05-08 23:41:26 -0700

    Heroku

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ let Employees = require('./employees.model');
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/employees', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/employees', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once('open', function() {
@@ -87,7 +87,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
   
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname,'build', 'index.html'));
+      res.sendFile(path.join(__dirname,'build', 'index.html'));
     });
   }
 
